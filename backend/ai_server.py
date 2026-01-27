8600477385691c82be1aa415e9c3923657b0b044
jerry7129 <jerry7129@naver.com>
2026-01-27 20:15:14 +0900

    경로 수정

--- backend/ai_server.py
+++ backend/ai_server.py
@@ -25,8 +25,8 @@ app = FastAPI()
 # BASE_S1_PATH = "/workspace/GPT_SoVITS/pretrained_models/custom_base/s1.ckpt"
 # BASE_S2_PATH = "/workspace/GPT_SoVITS/pretrained_models/custom_base/s2.pth"
 # Base Model Paths (Found in gsv-v2final-pretrained)
-BASE_S1_PATH = "/workspace/GPT_SoVITS/GPT_weights_v2Pro/xxx-e15.ckpt"
-BASE_S2_PATH = "/workspace/GPT_SoVITS/SoVITS_weights_v2Pro/xxx_e8_s96.pth"
+BASE_S1_PATH = "/workspace/GPT_weights_v2Pro/xxx-e15.ckpt"
+BASE_S2_PATH = "/workspace/SoVITS_weights_v2Pro/xxx_e8_s96.pth"
 
 class TrainRequest(BaseModel):
     user_id: str
